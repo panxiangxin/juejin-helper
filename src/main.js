f6ce271517ce73ad2d1179e606e15256ba6f6611
panxiangxin <www.721297578@qq.com>
2023-06-15 09:46:28 +0800

    fix: 去除 BUG Fix 功能

--- src/main.js
+++ src/main.js
@@ -4,24 +4,24 @@ const { wait, getRandomArbitrary } = require('./utils/utils.js')
 const { COOKIE } = require('./ENV.js')
 
 const growth = {
-  userName: '', // 用户名
-  checkedIn: false, // 是否签到
-  incrPoint: 0, // 签到获得矿石数
-  sumPoint: 0, // 总矿石数
-  contCount: 0, // 连续签到天数
-  sumCount: 0, // 累计签到天数
-  dippedLucky: false, // 是否沾喜气
-  dipValue: 0, // 幸运值
-  luckyValue: 0, // 总幸运值
-  freeCount: 0, // 免费抽奖次数
-  freeDrawed: false, // 是否免费抽奖
-  lotteryName: '', // 奖品名称
-  collectedBug: false, // 是否收集 Bug
-  collectBugCount: 0, // 收集 Bug 的数量
+    userName: '', // 用户名
+    checkedIn: false, // 是否签到
+    incrPoint: 0, // 签到获得矿石数
+    sumPoint: 0, // 总矿石数
+    contCount: 0, // 连续签到天数
+    sumCount: 0, // 累计签到天数
+    dippedLucky: false, // 是否沾喜气
+    dipValue: 0, // 幸运值
+    luckyValue: 0, // 总幸运值
+    freeCount: 0, // 免费抽奖次数
+    freeDrawed: false, // 是否免费抽奖
+    lotteryName: '', // 奖品名称
+    collectedBug: false, // 是否收集 Bug
+    collectBugCount: 0 // 收集 Bug 的数量
 }
 
 const message = () => {
-  return `
+    return `
 Hello ${growth.userName}
 ${growth.checkedIn ? `签到 +${growth.incrPoint} 矿石` : '今日已签到'}
 当前矿石数 ${growth.sumPoint}
@@ -36,89 +36,89 @@ ${growth.collectedBug ? `收集 Bug +${growth.collectBugCount}` : '暂无可收�
 }
 
 const main = async () => {
-  const juejin = new Juejin()
+    const juejin = new Juejin()
 
-  // 登录
-  try {
-    await juejin.login(COOKIE)
+    // 登录
+    try {
+        await juejin.login(COOKIE)
 
-    growth.userName = juejin.user.user_name
-  } catch {
-    throw new Error('登录失败, 请尝试更新 Cookies')
-  }
-
-  // 签到
-  const checkIn = await juejin.getTodayStatus()
-
-  if (!checkIn) {
-    const checkInResult = await juejin.checkIn()
-
-    growth.checkedIn = true
-    growth.incrPoint = checkInResult.incr_point
-  }
-
-  // 签到天数
-  const counts = await juejin.getCounts()
-
-  growth.contCount = counts.cont_count
-  growth.sumCount = counts.sum_count
-
-  // 沾喜气
-  const lotteryHistory = await juejin.getLotteryHistory()
-  const lotteries = lotteryHistory.lotteries || []
-
-  if (lotteries.length > 0) {
-    const [firstLottery] = lotteries
-    const dipLuckyResult = await juejin.dipLucky(firstLottery.history_id)
-
-    growth.dippedLucky = dipLuckyResult.has_dip
-    growth.dipValue = dipLuckyResult.dip_value
-    growth.luckyValue = dipLuckyResult.total_value
-  }
-
-  // 免费抽奖
-  const lotteryConfig = await juejin.getLotteryConfig()
-  growth.freeCount = lotteryConfig.free_count || 0
-
-  if (growth.freeCount > 0) {
-    const lottery = await juejin.drawLottery()
-
-    growth.freeDrawed = true
-    growth.lotteryName = lottery.lottery_name
-  }
-
-  // 当前矿石数
-  growth.sumPoint = await juejin.getCurrentPoint()
-
-  // BugFix
-  const notCollectBug = await juejin.getNotCollectBug()
-
-  if (notCollectBug.length > 0) {
-    const requests = notCollectBug.map(bug => {
-      return async () => {
-        await juejin.collectBug(bug)
-        await wait(getRandomArbitrary(1000, 1500))
-      }
-    })
-
-    for (const request of requests) {
-      await request()
-
-      growth.collectBugCount++
+        growth.userName = juejin.user.user_name
+    } catch {
+        throw new Error('登录失败, 请尝试更新 Cookies')
     }
 
-    growth.collectedBug = true
-  }
+    // 签到
+    const checkIn = await juejin.getTodayStatus()
 
-  pushMessage({
-    type: 'info',
-    message: message(),
-  })
+    if (!checkIn) {
+        const checkInResult = await juejin.checkIn()
+
+        growth.checkedIn = true
+        growth.incrPoint = checkInResult.incr_point
+    }
+
+    // 签到天数
+    const counts = await juejin.getCounts()
+
+    growth.contCount = counts.cont_count
+    growth.sumCount = counts.sum_count
+
+    // 沾喜气
+    const lotteryHistory = await juejin.getLotteryHistory()
+    const lotteries = lotteryHistory.lotteries || []
+
+    if (lotteries.length > 0) {
+        const [firstLottery] = lotteries
+        const dipLuckyResult = await juejin.dipLucky(firstLottery.history_id)
+
+        growth.dippedLucky = dipLuckyResult.has_dip
+        growth.dipValue = dipLuckyResult.dip_value
+        growth.luckyValue = dipLuckyResult.total_value
+    }
+
+    // 免费抽奖
+    const lotteryConfig = await juejin.getLotteryConfig()
+    growth.freeCount = lotteryConfig.free_count || 0
+
+    if (growth.freeCount > 0) {
+        const lottery = await juejin.drawLottery()
+
+        growth.freeDrawed = true
+        growth.lotteryName = lottery.lottery_name
+    }
+
+    // 当前矿石数
+    growth.sumPoint = await juejin.getCurrentPoint()
+
+    // BugFix
+    // const notCollectBug = await juejin.getNotCollectBug()
+
+    // if (notCollectBug.length > 0) {
+    //   const requests = notCollectBug.map(bug => {
+    //     return async () => {
+    //       await juejin.collectBug(bug)
+    //       await wait(getRandomArbitrary(1000, 1500))
+    //     }
+    //   })
+
+    //   for (const request of requests) {
+    //     await request()
+
+    //     growth.collectBugCount++
+    //   }
+
+    //   growth.collectedBug = true
+    // }
+
+    pushMessage({
+        type: 'info',
+        message: message()
+    })
 }
 
-main().catch(error => {
-  pushMessage({
-    type: 'error',
-    message: error.stack,
-  })
+main().catch((error) => {
+    pushMessage({
+        type: 'error',
+        message: error.stack
+    })
 })
